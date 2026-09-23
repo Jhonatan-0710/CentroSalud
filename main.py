486032ecf3950e2f7f94070da67731c4c4171b96
Jhonatan Smith Ramos Yarleque <jhonatan.sry0910@gmail.com>
2026-09-23 18:36:36 -0500

    Inicio del proyecto del sistema

---
+++ main.py
@@ -0,0 +1,6 @@
+print("==========================================")
+print(" SISTEMA CENTRO DE SALUD GANÍMEDES")
+print("==========================================")
+
+print("Sistema iniciado correctamente.")
+print("Proyecto de gestión asistencial y administrativa.")
